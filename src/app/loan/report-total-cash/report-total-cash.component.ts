55890ad13ff6f4059e18b5da4ee61dab1afbc9b0
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-07-09 14:04:29 -0600

    report, total cash

---
+++ src/app/loan/report-total-cash/report-total-cash.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-report-total-cash',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './report-total-cash.component.html',
+  styleUrls: ['./report-total-cash.component.css']
+})
+export class ReportTotalCashComponent {
+
+}
