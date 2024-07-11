75156e1b06c9769dc84a9eca17353978e10b64b6
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-07-11 02:48:58 -0600

    Report student more sanctions payment

---
+++ src/app/loan/report-student-payment-sanction/report-student-payment-sanction.component.ts
@@ -0,0 +1,44 @@
+import {Component, inject, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoanService} from "../service/loan.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {reportMoreLoansCareer, reportStudentPaymentSanction} from "../../../model";
+import {PaymentService} from "../../payment/service/payment.service";
+import {MessageService, SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {ToastModule} from "primeng/toast";
+
+@Component({
+  selector: 'app-report-student-payment-sanction',
+  standalone: true,
+  imports: [CommonModule, SharedModule, TableModule, ToastModule],
+  providers:[MessageService],
+  templateUrl: './report-student-payment-sanction.component.html',
+  styleUrls: ['./report-student-payment-sanction.component.css']
+})
+export class ReportStudentPaymentSanctionComponent  implements OnInit{
+  private loanService = inject(PaymentService);
+  private router = inject(Router);
+  private messageService = inject(MessageService);
+  studentPaymentsSanction!:reportStudentPaymentSanction;
+  private carnet!:string;
+  private init!:string;
+  private end!:string
+  private route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.carnet = params['carnet'];
+      this.init = params['init'];
+      this.end = params['end'];
+      this.loanService.getReport({carnet:this.carnet,init: this.init, end: this.end}).subscribe(
+        response => {
+          this.studentPaymentsSanction = response;
+        },
+        error => {
+          this.messageService.add({severity:'error',summary:'Error',detail: error.error})
+        }
+      )
+    });
+  }
+}
