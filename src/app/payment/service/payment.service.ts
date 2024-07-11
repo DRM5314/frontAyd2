88503e65f8335c3dfba7380f87f913a5b4f2f1ce
DRM5314 <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2024-07-11 02:50:05 -0600

    payment service, for save and report

---
+++ src/app/payment/service/payment.service.ts
@@ -0,0 +1,23 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment.development";
+import {reportStudentPaymentSanction} from "../../../model";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  private url =`${this.baseUrl}/lb/api/payment`;
+  constructor() { }
+  public postPayment(postData:any):Observable<reportStudentPaymentSanction>{
+    return this.http.post<reportStudentPaymentSanction>(`${this.url}`,postData);
+  }
+  public getReport(postData:any):Observable<reportStudentPaymentSanction>{
+    console.log(postData);
+    return this.http.post<reportStudentPaymentSanction>(`${this.url}/more-student`,postData);
+  }
+
+}
